db77508d3607865996c40fa940d4410b81ca9794
QMHLO <htetlinoo@qualymm.com>
2023-06-27 13:47:29 +0630

    chat-added]

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import logo from "./logo.svg";
+import React from "react";
+import SingIn from "./compoments/SingIn";
+import Header from "./compoments/Header";
+import SignUp from "./compoments/SignUp";
+import { AuthContext } from "./Context/AuthContext";
+import { Routes, Route, Navigate } from "react-router";
+import HomePage from "./compoments/HomePage";
+import DetailPage from "./compoments/DetailPage";
+import ReactChat from "./compoments/ReactChat";
+import OwnerChat from "./compoments/OwnerChat";
 import "./App.css";
-import OwnerChat from "./OwnerChat";
 
 function App() {
+  const { currentUser, adminUser } = React.useContext(AuthContext);
+  const [jwt, setJWT] = React.useState(localStorage.getItem("jwt-token"));
+  const [admin, setAdmin] = React.useState(localStorage.getItem("admin"));
+
+  console.log(`JWT TOKEN ${jwt}`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-      <OwnerChat />
+    <div>
+      <Header />
+
+      {(!currentUser || !adminUser) && (
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SingIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          {/* <Route path="/detail" element={<DetailPage />} /> */}
+          {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
+          <Route path="/detail/:id" element={<DetailPage />} />
+        </Routes>
+      )}
+      {(currentUser || jwt) && (!adminUser || !admin) && (
+        <Routes>
+          <Route path="/chat" element={<ReactChat />} />
+          <Route path="/adminchat" element={<OwnerChat />} />
+        </Routes>
+      )}
+      {(adminUser || admin) && (
+        <Routes>
+          <Route path="/adminchat" element={<OwnerChat />} />
+          <Route path="/chat" element={<Navigate replace to="/adminchat" />} />
+        </Routes>
+      )}
     </div>
   );
 }
