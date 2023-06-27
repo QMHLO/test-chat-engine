8cbdbb2371a6032345ea4557c873eb6467840f08
QMHLO <htetlinoo@qualymm.com>
2023-06-27 13:14:21 +0630

    initial

---
+++ src/OwnerChat.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+
+function OwnerChat() {
+  return <ChatEngine projectID="7e806a50-45ed-494b-9598-dd683a3324b4" userName="chat owner" userSecret="QualyMM1234" />;
+}
+
+export default OwnerChat;
